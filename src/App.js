3e4e4f4ac685de1004532c6709c1f2b473bd0f07
yesthis1please <yesthis1please@gmail.com>
2022-05-27 20:40:06 +0900

    I added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      <h1>Weather Application</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.instagram.com/bara_annamaria/" target="_blank">
+          Annamaria Bara
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/yesthis1please/shecodes_react_weather_codealong"
+          target="_blank"
+        >
+          open sourced on Github
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
