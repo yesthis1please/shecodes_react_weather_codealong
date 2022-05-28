0b5b435851be1745c06f93b28bfb057b9af6d3c7
yesthis1please <yesthis1please@gmail.com>
2022-05-28 22:08:10 +0900

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.instagram.com/bara_annamaria/" target="_blank">
-          Annamaria Bara
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/yesthis1please/shecodes_react_weather_codealong"
-          target="_blank"
-        >
-          open sourced on Github
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.instagram.com/bara_annamaria/" target="_blank">
+            Annamaria Bara
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/yesthis1please/shecodes_react_weather_codealong"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
